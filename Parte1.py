e59df87dd0ebe2c55d46983aaa945d07ff415acd
Musstela <arthureco.maia@gmail.com>
2024-06-26 00:46:19 -0300

    feito

--- Parte1.py
+++ Parte1.py
@@ -5,33 +5,33 @@ from math import gcd as bltin_gcd
 Ep = int("2E76A0094D4CEE0AC516CA162973C895",16)
 Np = int("1985008F25A025097712D26B5A322982B6EBAFA5826B6EDA3B91F78B7BD63981382581218D33A9983E4E14D4B26113AA2A83BBCCFDE24310AEE3362B6100D06CC1EA429018A0FF3614C077F59DE55AADF449AF01E42ED6545127DC1A97954B89729249C6060BA4BD3A59490839072929C0304B2D7CBBA368AEBC4878A6F0DA3FE58CECDA638A506C723BDCBAB8C355F83C0839BF1457A3B6B89307D672BBF530C93F022E693116FE4A5703A665C6010B5192F6D1FAB64B5795876B2164C86ABD7650AEDAF5B6AFCAC0438437BB3BDF5399D80F8D9963B5414EAFBFA1AA2DD0D24988ACECA8D50047E5A78082295A987369A67D3E54FFB7996CBE2C5EAD794391",16)
 
-print("Geração da Parte Assimetrica \n")
+# Geração da Parte Assimetrica
 p = sympy.randprime(2**1023, 2**1024)
 q = sympy.randprime(2**1023, 2**1024)
 
-print("p = "+str(p))
-print("\nq = "+str(q))
+print("\n p = "+str(p))
+print("\n q = "+str(q))
 
 modulo = p*q
 funcaoEuler = (p-1)*(q-1)
 
-print("\nmodulo = "+str(modulo))
-print("\nfuncaoEuler = "+str(funcaoEuler))
+print("\n modulo = "+str(modulo))
+print("\n funcaoEuler = "+str(funcaoEuler))
 
 inverso = 11111222223333334444445555556666661111188887
+print("\n inverso = "+str(inverso))
 
 primoRelativo = bltin_gcd(funcaoEuler, inverso) == 1
-print("\n" + str(primoRelativo))
+print("\n é primo relativo? " + str(primoRelativo))
 
 
-print("Geração da Parte Assimetrica \n")
-
+# Gerar chave simétrica
 inteiro128bits = 35966238280343333638208114881239354942
-#int.from_bytes(secrets.token_bytes(16), byteorder='big') #transforma em inteiro
 
+# Cifrar chave simétrica
 x = pow(inteiro128bits,Ep,Np)
+print("\n x = "+str(x))
+
+# Assinar	texto	cifrado
 sigX = pow(x,inverso,modulo)
-
-print(sigX)
-print("aham")
-
+print("\n sigX = "+str(sigX))
