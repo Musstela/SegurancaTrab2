46c3481637d4b86d8333e8a2d6ebb206bb3c1e35
Musstela <arthureco.maia@gmail.com>
2024-06-30 20:45:51 -0300

    parte 1 FEITA

--- Parte1.py
+++ Parte1.py
@@ -1,9 +1,8 @@
 import sympy
-import secrets
-from math import gcd as bltin_gcd
+import random
+import sys
 
-Ep = int("2E76A0094D4CEE0AC516CA162973C895",16)
-Np = int("1985008F25A025097712D26B5A322982B6EBAFA5826B6EDA3B91F78B7BD63981382581218D33A9983E4E14D4B26113AA2A83BBCCFDE24310AEE3362B6100D06CC1EA429018A0FF3614C077F59DE55AADF449AF01E42ED6545127DC1A97954B89729249C6060BA4BD3A59490839072929C0304B2D7CBBA368AEBC4878A6F0DA3FE58CECDA638A506C723BDCBAB8C355F83C0839BF1457A3B6B89307D672BBF530C93F022E693116FE4A5703A665C6010B5192F6D1FAB64B5795876B2164C86ABD7650AEDAF5B6AFCAC0438437BB3BDF5399D80F8D9963B5414EAFBFA1AA2DD0D24988ACECA8D50047E5A78082295A987369A67D3E54FFB7996CBE2C5EAD794391",16)
+sys.setrecursionlimit(1500)
 
 def mdc_extendido(a, b):
     if a == 0:
@@ -16,23 +15,18 @@ def mdc_extendido(a, b):
 def inverso_modular(Ea, L):
     gcd, x, y = mdc_extendido(Ea, L)
     if gcd != 1:
-        raise ValueError(f"O inverso modular não existe para Ea = {Ea} e L = {L}")
+        return None
     else:
         return x % L
 
-def mdc(a, b):
-    while b:
-        a, b = b, a % b
-    return a
-
-def encontrar_primo_relativo(L):
-    candidato = 2
+def gerar_primo_relativo_com_inverso_128_bits(L):
     while True:
-        if mdc(candidato, L) == 1:
-            return candidato
-        candidato += 1
+        Ea = random.randint(2, L-1)
+        Da = inverso_modular(Ea, L)
+        if Da is not None and Da.bit_length() >= 128:
+            return Ea, Da
 
-# Geração da Parte Assimetrica
+# Gerar p e q como grandes números primos de 1024 bits
 p = sympy.randprime(2**1023, 2**1024)
 q = sympy.randprime(2**1023, 2**1024)
 
@@ -40,15 +34,14 @@ print("\n p = "+hex(p)[2:])
 print("\n q = "+hex(q)[2:])
 
 modulo = p*q
-funcaoEuler = (p-1)*(q-1)
+funcaoEuler_L = (p-1)*(q-1)
 
 print("\n modulo = "+hex(modulo)[2:])
-print("\n funcaoEuler = "+hex(funcaoEuler)[2:])
+print("\n funcaoEuler_L = "+hex(funcaoEuler_L)[2:])
 
-Ea = encontrar_primo_relativo(funcaoEuler)
+Ea,Da = gerar_primo_relativo_com_inverso_128_bits(funcaoEuler_L)
+
 print("\n Ea = "+hex(Ea)[2:])
-
-Da = inverso_modular(Ea,modulo)
 print("\n Da = " + hex(Da)[2:])
 
 
